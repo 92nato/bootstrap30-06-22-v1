921ca7983e629b9a3227846fb514c39918e3cf27
92nato <nato6661992@gmail.com>
2022-06-30 16:28:55 -0300

    modify app.js to include images by id, editing and modify style.css and modify index.html in crad sections and add ID in img randomCats on Random Cats img tag

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ const URL ='https://api.thecatapi.com/v1/images/search';
 fetch(URL)
     .then(res => res.json())
     .then(data => {
-        const img = document.querySelector('img');
+        const img = document.getElementById("randomCats");
         img.src = data[0].url
     });
